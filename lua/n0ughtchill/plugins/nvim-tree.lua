be523082c3c7b157ab367abcd2ef4f5aace7738b
n0ughtchill <59179202+n0ughtchill@users.noreply.github.com>
2022-11-25 11:30:39 -0600

    auto open nvimtree and close on file open

--- lua/n0ughtchill/plugins/nvim-tree.lua
+++ lua/n0ughtchill/plugins/nvim-tree.lua
@@ -1,6 +1,6 @@
 local setup, nvimtree = pcall(require, "nvim-tree")
 if not setup then
-  return
+	return
 end
 
 -- recommended settings from nvim-tree documentation
@@ -8,21 +8,30 @@ vim.g.loaded = 1
 vim.g.loaded_netrwPlugin = 1
 
 nvimtree.setup({
-  renderer = {
-    icons = {
-      glyphs = {
-        folder = {
-          arrow_closed = " ", -- arrow when folder is closed
-          arrow_open = " ",
-        },
-      },
-    },
-  },
-  actions = {
-    open_file = {
-      window_picker = {
-        enable = false,
-      },
-    },
-  },
+	open_on_setup = true,
+	view = {
+		adaptive_size = true,
+	},
+	renderer = {
+		highlight_git = true,
+		icons = {
+			show = {
+				git = true,
+			},
+			glyphs = {
+				folder = {
+					arrow_closed = " ", -- arrow when folder is closed
+					arrow_open = " ",
+				},
+			},
+		},
+	},
+	actions = {
+		open_file = {
+			quit_on_open = true,
+			window_picker = {
+				enable = false,
+			},
+		},
+	},
 })
